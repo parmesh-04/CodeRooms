a308982e1fa70602588500b9e98da6d25bb428a2
parmesh-04 <parmeshg4000@gmail.com>
2024-11-07 12:24:28 +0530

    rating added

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express=require("express");
-const { registerUser, loginUser, logoutUser } = require("../controllers/userController");
+const { registerUser, loginUser, logoutUser } = require("../controller/userController");
 const { isAuthenticated } = require("../middleware/auth");
 const router=express.Router();
 
@@ -8,7 +8,7 @@ const router=express.Router();
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
 router.route("/logout").get(isAuthenticated,logoutUser)
-router.route("/filter/")
+router.route("/rating").get()
 
 
 
